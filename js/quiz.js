b92123c7a9eca25a85c53d90bad4d03d3c77474e
fhs47805 <alymalym@web.de>
2021-10-26 16:05:00 +0200

    Alternative added

--- js/quiz.js
+++ js/quiz.js
@@ -1,14 +1,20 @@
+"use strict"
 import { questions } from './questions.js'
 
-let question = questions[Math.floor(Math.random()*questions.length)];
-
 export function askQuestion () {
-    return question.question +
+    let question = questions[Math.floor(Math.random()*questions.length)];
+    const { correctAnswer, ...q } = question;
+    return q;
+    /* Alternative..
+        return question.question +
         "a: " + question.a + "\n" +
         "b: " + question.b + "\n" +
         "c: " + question.c + "\n" +
-        "d: " + question.d;
+        "d: " + question.d;*/
 }
 export function answerQuestion (q, answer) {
-    return question.correctAnswer === answer;
+    //Find question
+    let searchedQuestion = questions.find(element => element.question === q.question)
+
+    return searchedQuestion.correctAnswer === answer;
 }
